13ed9fb6498775d92476a8942584e9470a7888b9
wshcdr <wshcdr@eyou.com>
2015-03-04 13:07:11 +0800

    sample
    first commit

---
+++ event_sink001/event_sink001/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using event_sink001;
+
+namespace event_sink001
+{
+    class test 
+    {
+        public void on_error(int i)
+        {
+            Console.WriteLine(i);
+        }
+        public void on_status(int i)
+        {
+
+        }
+
+        public void test_evt()
+
+        {
+            event_source1.Class1 obj = new event_source1.Class1();
+            obj.evt_handler += new event_source1.Class1.del_type(obj_evt_handler);
+            obj.Fun("hello");
+        }
+
+        public void obj_evt_handler(string str)
+        {
+            Console.WriteLine(str);
+        }
+    }
+    class Program 
+    {
+        
+        static void Main(string[] args)
+        {
+            test obj = new test();
+            obj.test_evt();
+            
+        }
+    }
+}
---
+++ event_source1/event_source1/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace event_source1
+{
+    public class Class1
+    {
+        public delegate void del_type(string name);
+
+        public event del_type evt_handler;
+
+
+        public void Fun(string name) 
+        {
+            if (evt_handler != null) 
+            {
+                evt_handler(name + " from source");
+            }
+        }
+    }
+}
